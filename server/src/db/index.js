0b343c7046ff19140e2ddbad5b01dd084d0f7fb6
Jallah-lj <lawuobahjallah79@gmail.com>
2026-02-01 16:35:02 +0000

    Fix: Force IPv4 DNS resolution for Render deployment

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,5 +1,9 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
+import dns from 'node:dns';
+
+// Force IPv4 to avoid ENETUNREACH errors on Render (IPv6 not supported)
+dns.setDefaultResultOrder('ipv4first');
 
 dotenv.config();
 
